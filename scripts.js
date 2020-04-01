9f71fa1e5104e22eb4868e527dd960d8597adc6e
tom10271 <aokaddaoc@gmail.com>
2020-04-01 19:56:00 +0800

    修復料理時未能一次弄好錯誤丟掉問題

--- scripts.js
+++ scripts.js
@@ -7,48 +7,69 @@ function l(u, i) {
         d.body.appendChild(s);
     }
 }
+
 l('//code.jquery.com/jquery-3.4.1.min.js', 'jquery');
+
 function isIn(urlMatchRegex) {
     return window.location.href.match(urlMatchRegex);
 }
+
 const cookItems = {
     greenTea: '緑茶',
     soup: '白玉清湯'
 };
 let cookItem = '';
-console.log(chrome.storage);
-chrome.storage.local.get('cookItem', function(result) {
+
+if (!('storage' in chrome)) {
+    alert('chrome.storage does not exist');
+}
+
+console.log({ chrome, storage: chrome.storage });
+chrome.storage.local.get('cookItem', function (result) {
+    console.log({ cookItem: result });
+
     cookItem = 'cookItem' in result ? result.cookItem : cookItems.greenTea;
+
+    console.log({cookItem});
     if (isIn(/garden_teatable/)) {
         if (isIn(/garden_teatable_select_recipe/)) {
             if ($('#result').length) {
                 var nOutOfN = $('#extension span:last-child').text().split('/');
+
                 if (nOutOfN[0] === nOutOfN[1]) {
+                    console.log('身上料理已滿');
+
                     if (cookItem === cookItems.soup) {
-                        $('#popup input.btn').click();
+                        debugger;$('#popup input.btn').click();
                     } else {
-                        window.location.href = 'http://msp.musou-online.jp/m/garden_top';
+                        debugger;window.location.href = 'http://msp.musou-online.jp/m/garden_top';
                     }
                 } else {
+                    const $continueCook = $("a[href^='/m/garden_teatable_blend_confirm']");
                     const $eatPopup = $('#popup_eat input.btn');
-                    if ($eatPopup.length > 0) {
-                        $eatPopup.click();
+
+                    if ($continueCook.length > 0) {
+                        debugger;$continueCook.click();
+
+                        debugger;window.location.href = $continueCook.attr('href');
+                    } else if ($eatPopup.length > 0) {
+                        debugger;$eatPopup.click();
                     } else {
-                        $('#popup input.btn').click();
+                        debugger;$('#popup input.btn').click();
                     }
                 }
             } else {
                 // Find Green tea and select it
-                $(`span.list_name:contains(${cookItem})`).closest("[id^=radioitem]").click();
+                debugger;$(`span.list_name:contains(${cookItem})`).closest("[id^=radioitem]").click();
                 setTimeout(() => {
-                    $('#recipe_select_btn').click();
+                    debugger;$('#recipe_select_btn').click();
                 }, 500);
             }
         }
         if (isIn(/garden_teatable_blend_confirm/)) {
-            $('a[onclick]').click();
+            debugger;$('a[onclick]').click();
             setTimeout(() => {
-                $('input.btn').click();
+                debugger;$('input.btn').click();
             }, 500);
         }
         if (isIn(/garden_teatable_result/)) {
@@ -57,14 +78,14 @@ chrome.storage.local.get('cookItem', function(result) {
                     chrome.storage.local.set({
                             'cookItem': cookItems.greenTea
                         },
-                        function(data) {
-                            window.location.href = 'http://msp.musou-online.jp/m/garden_top';
+                        function (data) {
+                            debugger;window.location.href = 'http://msp.musou-online.jp/m/garden_top';
                         }
                     );
                 }
-                window.location.href = 'http://msp.musou-online.jp/m/garden_top';
+                debugger;window.location.href = 'http://msp.musou-online.jp/m/garden_top';
             } else {
-                window.location.href = 'http://msp.musou-online.jp/m/garden_teatable_select_recipe?recipelisttype=2';
+                debugger;window.location.href = 'http://msp.musou-online.jp/m/garden_teatable_select_recipe?recipelisttype=2';
             }
         }
         if (isIn(/garden_teatable_food_effect/)) {
@@ -77,15 +98,15 @@ chrome.storage.local.get('cookItem', function(result) {
                 chrome.storage.local.set({
                         'cookItem': cookItems.soup
                     },
-                    function(data) {
-                        window.location.href = 'http://msp.musou-online.jp/m/garden_teatable_select_recipe?recipelisttype=2';
+                    function (data) {
+                        debugger;window.location.href = 'http://msp.musou-online.jp/m/garden_teatable_select_recipe?recipelisttype=2';
                     }
                 );
             }
         }
         // Remove those cannot fill water
         $('#plant_list >[id^=plant_].disabled')
-            .map(function(i, each) {
+            .map(function (i, each) {
                 return each['id'].replace('plant_', '');
             })
             .get()
@@ -93,11 +114,11 @@ chrome.storage.local.get('cookItem', function(result) {
                 $('#plant_list #plant_' + each).remove();
                 $('#plant_list #plant_info_' + each).remove();
             });
-        $('#plant_list >[id^=plant_]:first').click();
+        debugger;$('#plant_list >[id^=plant_]:first').click();
         const intervalId = setInterval(() => {
             const $form = $('#plant_list >[id^=plant_info_] form[action="/m/garden_plant_water"]');
             if ($form.length) {
-                $form.find('input.btn').click();
+                debugger;$form.find('input.btn').click();
                 clearInterval(intervalId);
             }
         });
@@ -112,10 +133,10 @@ chrome.storage.local.get('cookItem', function(result) {
     if (isIn(/garden_plant_water/)) {
         const stillHaveGreenTea = $('.explanation').text().match(/緑茶/);
         if (stillHaveGreenTea && stillHaveGreenTea[0] === '緑茶') {
-            window.location.href = 'http://msp.musou-online.jp/m/garden_top';
+            debugger;window.location.href = 'http://msp.musou-online.jp/m/garden_top';
         } else {
             // If action point went low, change to make soup
-            window.location.href = 'http://msp.musou-online.jp/m/garden_teatable_select_recipe?recipelisttype=2';
+            debugger;window.location.href = 'http://msp.musou-online.jp/m/garden_teatable_select_recipe?recipelisttype=2';
         }
     }
 });
